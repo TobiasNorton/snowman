75ff54c6663ebd5f9d91958a16a77a8a08dae1e5
Tobias Norton <tnorton20@yahoo.com>
2018-11-24 15:44:06 -0500

    Basic interface set up with CSS

--- src/App.js
+++ src/App.js
@@ -1,28 +1,73 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import words from './words.json'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      keyboard: [
+        ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
+        ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'],
+        ['z', 'x', 'c', 'v', 'b', 'n', 'm']
+      ],
+      snowmanStep: 0
+    }
+  }
+
+  setSnowman = () => {}
+
+  // generateRandomWord = () => {}
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1>Build A Snowman</h1>
+        {/* {generateRandomWord()} */}
+        <div>
+          <img className="snowman-image" src="./snowman_images/step_7.png" />
+
+          <p className="word">Word Goes Here</p>
+
+          <section className="keyboard">
+            <div className="first-row">
+              <button onClick>q</button>
+              <button>w</button>
+              <button>e</button>
+              <button>r</button>
+              <button>t</button>
+              <button>y</button>
+              <button>u</button>
+              <button>i</button>
+              <button>o</button>
+              <button>p</button>
+            </div>
+            <div className="second-row">
+              <button>a</button>
+              <button>s</button>
+              <button>d</button>
+              <button>f</button>
+              <button>g</button>
+              <button>h</button>
+              <button>j</button>
+              <button>k</button>
+              <button>l</button>
+            </div>
+            <div className="third-row">
+              <button>z</button>
+              <button>x</button>
+              <button>c</button>
+              <button>v</button>
+              <button>b</button>
+              <button>n</button>
+              <button>m</button>
+            </div>
+          </section>
+        </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
